18a1e84af5fad297465f874515f12a149dc74d0f
fit <l.s.rekha.29@gmail.com>
2018-02-14 13:17:19 +0530

    complete design

--- app/src/main/java/com/example/ammulu/waterbilling/SubBillingActivity.java
+++ app/src/main/java/com/example/ammulu/waterbilling/SubBillingActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class SubBillingActivity extends AppCompatActivity {
-     Button bill,collection,validate;
+     Button bill,collection,validate,billback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +16,7 @@ public class SubBillingActivity extends AppCompatActivity {
         bill=(Button)findViewById(R.id.billing);
         collection=(Button)findViewById(R.id.collection);
         validate=(Button)findViewById(R.id.validate);
+        billback=(Button)findViewById(R.id.subbillback);
         bill.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,6 +36,13 @@ public class SubBillingActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(getApplicationContext(),ValidateActivity.class);
+                startActivity(i);
+            }
+        });
+        billback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),HomeActivity.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/ammulu/waterbilling/ValidateActivity.java
+++ app/src/main/java/com/example/ammulu/waterbilling/ValidateActivity.java
@@ -7,13 +7,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class ValidateActivity extends AppCompatActivity {
-Button vbilling,vcollection;
+Button vbilling,vcollection,vback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validate);
         vbilling=(Button)findViewById(R.id.vbilling);
         vcollection=(Button)findViewById(R.id.vcollection);
+        vback=(Button)findViewById(R.id.vback);
         vbilling.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -25,6 +26,14 @@ Button vbilling,vcollection;
             @Override
             public void onClick(View v) {
                 Intent i=new Intent(getApplicationContext(),ValidateCollectionActivity.class);
+                startActivity(i);
+
+            }
+        });
+        vback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),SubBillingActivity.class);
                 startActivity(i);
 
             }
