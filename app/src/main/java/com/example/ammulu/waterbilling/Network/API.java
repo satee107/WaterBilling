0fa973cb0054e87cb3dcd2576bba28f12237747e
fit <l.s.rekha.29@gmail.com>
2018-03-31 17:46:21 +0530

    Billing validate design

--- app/src/main/java/com/example/ammulu/waterbilling/Network/API.java
+++ app/src/main/java/com/example/ammulu/waterbilling/Network/API.java
@@ -8,4 +8,6 @@ public class API {
     public  static String logincredentialsurl="http://www.globalmrbs.com/mrbs/buildinglogin.php";
     public  static String canDetailsurl="http://www.globalmrbs.com/mrbs/addreading.php";
     public  static String billCollectUrl="http://www.globalmrbs.com/mrbs/addamount.php";
+    public  static String getreagingsUrl="http://www.globalmrbs.com/mrbs/getreadings.php";
+    public  static String getamountstUrl="http://www.globalmrbs.com/mrbs/getamounts.php";
 }
