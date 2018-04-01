57acadb05bd121f46e5c4faf8908415f89444051
Rekha projects <l.s.rekha.29@gmail.com>
2018-04-01 23:22:06 +0530

    validate bill and collection

--- app/src/main/java/com/example/ammulu/waterbilling/ValidateBillingActivity.java
+++ app/src/main/java/com/example/ammulu/waterbilling/ValidateBillingActivity.java
@@ -1,14 +1,103 @@
 package com.example.ammulu.waterbilling;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.ammulu.waterbilling.Network.API;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ValidateBillingActivity extends AppCompatActivity {
-
+      TextView biddata,candata,flatnodata,readingdata,readingdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validate_billing);
+        biddata=findViewById(R.id.biddata);
+        candata=findViewById(R.id.candata);
+        flatnodata=findViewById(R.id.flatnodata);
+        readingdata=findViewById(R.id.readingdata);
+        readingdate=findViewById(R.id.readingdate);
+        getValidateBilling();
+
+    }
+
+    private void getValidateBilling() {
+//        if(ConnectivityReceiver.isConnected()==false){
+//            checkConnection();
+//            // displayMobileDataSettingsDialog(MainActivity.this);
+//        }
+      //  checkConnection();
+        // progressbar.setVisibility(View.VISIBLE);
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String serverURL = API.getreagingsUrl;
+//        String uname=etname.getText().toString().trim();
+//        String upwd=etpwd.getText().toString().trim();
+       // String url = serverURL+"?uname="+uname+"&upass="+upwd;
+        final StringRequest getRequest = new StringRequest(Request.Method.GET, serverURL,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            // Getting JSON Array node
+                            JSONArray arr = jsonObj.getJSONArray("readings");
+                            for(int i=0;i<arr.length();i++){
+                                JSONObject obj= arr.getJSONObject(i);
+
+                                String sbid = obj.getString("buildingid");
+                                String scanno = obj.getString("canno");
+                                String sflatno = obj.getString("flatno");
+                                String sreading = obj.getString("reading");
+                                String sreadingdate = obj.getString("readingdate");
+
+                                biddata.setText(sbid);
+                                candata.setText(scanno);
+                                flatnodata.setText(sflatno);
+                                readingdata.setText(sreading);
+                                readingdate.setText(sreadingdate);
+
+                            }
+
+                        } catch (final JSONException e) {
+                            Log.e("MainActivity", "Json parsing error: " + e.getMessage());
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                },
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Toast.makeText(getApplicationContext(), "No Network Connection", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() {
+                Toast.makeText(ValidateBillingActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                return null;
+            }
+        };
+        queue.add(getRequest);
     }
 }
--- app/src/main/java/com/example/ammulu/waterbilling/ValidateCollectionActivity.java
+++ app/src/main/java/com/example/ammulu/waterbilling/ValidateCollectionActivity.java
@@ -3,12 +3,94 @@ package com.example.ammulu.waterbilling;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.ammulu.waterbilling.Network.API;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
 
 public class ValidateCollectionActivity extends AppCompatActivity {
-
+     TextView cbid,cflatno,camount,cpaid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validate_collection);
+        cbid=findViewById(R.id.collbiddata);
+        cflatno=findViewById(R.id.collflatnodata);
+        camount=findViewById(R.id.collamountdata);
+        cpaid=findViewById(R.id.collpaiddate);
+
+        getValidateCollections();
+    }
+    private void getValidateCollections() {
+//        if(ConnectivityReceiver.isConnected()==false){
+//            checkConnection();
+//            // displayMobileDataSettingsDialog(MainActivity.this);
+//        }
+        //  checkConnection();
+        // progressbar.setVisibility(View.VISIBLE);
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String serverURL = API.getamountstUrl;
+//        String uname=etname.getText().toString().trim();
+//        String upwd=etpwd.getText().toString().trim();
+        // String url = serverURL+"?uname="+uname+"&upass="+upwd;
+        final StringRequest getRequest = new StringRequest(Request.Method.GET, serverURL,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            // Getting JSON Array node
+                            JSONArray arr = jsonObj.getJSONArray("amounts");
+                            for(int i=0;i<arr.length();i++){
+                                JSONObject obj= arr.getJSONObject(i);
+
+                                String sbid = obj.getString("bid");
+                                String sflatno = obj.getString("flatno");
+                                String samount = obj.getString("amount");
+                                String spaiddate = obj.getString("paiddate");
+
+                                cbid.setText(sbid);
+                                cflatno.setText(sflatno);
+                                camount.setText(samount);
+                                cpaid.setText(spaiddate);
+
+                            }
+
+                        } catch (final JSONException e) {
+                            Log.e("MainActivity", "Json parsing error: " + e.getMessage());
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                },
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Toast.makeText(getApplicationContext(), "No Network Connection", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() {
+                Toast.makeText(ValidateCollectionActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                return null;
+            }
+        };
+        queue.add(getRequest);
     }
 }
